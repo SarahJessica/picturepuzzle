730fb08ae29afc54c2cfda4b9c6f01e7513c9d06
Jessica Leivers <sarahjessicaleivers.com>
2015-04-11 09:59:27 -0700

    game basically working

--- client/index.js
+++ client/index.js
@@ -7,12 +7,13 @@ $(document).ready(init);
 //var difficulty = 4;
 
 function init(){
-  $('#start').click(gridSetup);
+  // $('#start').click(gridSetup);
   // gridSetup();
   $('td').click(revealImg);
+  $('#start').click($('td').css('background-color', 'pink'));
 }
 
-console.log($('.1').css('background-image').split('/').pop().slice(0, -1));
+// console.log($('.1').css('background-image').split('/').pop().slice(0, -1));
 // $('td').each(function(){
 
 // function gridSetup(){
@@ -36,13 +37,26 @@ function revealImg(){
 }
 
 function winCheck(){
-  var win = 0;
-  $('td').each(function(){
-  console.log($('td').css('background-image').split('/').pop().slice(0, -1)) === ($('td').className) + '.jpg' ? win++ : null;
-  }
-  win === difficulty*difficulty ? console.log("game won") : console.log("not yet");
+  // var win = 0;
+  // $('td').each(function(){
+  // console.log($('td').css('background-image').split('/').pop().slice(0, -1)) === ($('td').className) + '.jpg' ? win++ : null;
+  // }
+  // win === difficulty*difficulty ? console.log("game won") : console.log("not yet ");
   // $('td').each(function(){
   //   $('td').className === $('td').css('background-image', ) ? win++ : win;
   // })
-
-}
+  console.log('this function is called');
+  if(
+    $('#1').css('background-image') === "url(http://localhost:3000/assets/3/1.jpg)" &&
+    $('#2').css('background-image') === "url(http://localhost:3000/assets/3/2.jpg)" &&
+    $('#3').css('background-image') === "url(http://localhost:3000/assets/3/3.jpg)" &&
+    $('#4').css('background-image') === "url(http://localhost:3000/assets/3/4.jpg)" &&
+    $('#5').css('background-image') === "url(http://localhost:3000/assets/3/5.jpg)" &&
+    $('#6').css('background-image') === "url(http://localhost:3000/assets/3/6.jpg)" &&
+    $('#7').css('background-image') === "url(http://localhost:3000/assets/3/7.jpg)" &&
+    $('#8').css('background-image') === "url(http://localhost:3000/assets/3/8.jpg)" &&
+    $('#9').css('background-image') === "url(http://localhost:3000/assets/3/9.jpg)"
+  ) {
+    alert('you won!');
+    }
+ }
