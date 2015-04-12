89bd39cee8c841db85c0229798785277b2afbd3e
Jessica Leivers <sarahjessicaleivers.com>
2015-04-12 15:01:48 -0700

    scalable win function working

--- client/index.js
+++ client/index.js
@@ -4,11 +4,11 @@ $(document).ready(init);
 // var easyArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // var difficulty = $('#difficulty option:selected').val();
 // console.log(difficulty);
-var difficulty = 4;
+var difficulty = 3;
 
 function init(){
   // $('#start').click(gridSetup);
-  // gridSetup();
+ gridSetup();
   $('td').click(revealImg);
   $('#start').click($('td').css('background-color', 'pink'));
 }
@@ -27,19 +27,32 @@ function gridSetup(){
       counter++;
       $tr.append($td);
     }
+    console.log('$tr', $tr);
     $('#puzzle').append($tr);
   }
   $('td').css('background-color','#d69295');
-  console.log("you're in gameSetup");
+  // console.log("you're in gameSetup");
 }
 
 function revealImg(){
   var show = Math.floor(Math.random()*9) + 1;
-  $(this).css('background-image', 'url("/assets/3/' + show + '.jpg")');
+  $(this).css('background-image', 'url("/assets/' + difficulty + '/' + show + '.jpg")');
   winCheck();
 }
 
-function winCheck(){
+function winCheck() {
+  var matchCount = 0;
+
+  for (var i = (difficulty*difficulty); i > 0; i--){
+    if ( $('td#' + i).css('background-image').split('/').pop() === (i + '.jpg)')){
+      matchCount++;
+    } else {
+      break;
+    }
+  }
+
+  matchCount === (difficulty*difficulty) ? alert('you won!'): console.log('not yet');
+}
   // var win = 0;
   // $('td').each(function(){
   // console.log($('td').css('background-image').split('/').pop().slice(0, -1)) === ($('td').className) + '.jpg' ? win++ : null;
@@ -48,18 +61,18 @@ function winCheck(){
   // $('td').each(function(){
   //   $('td').className === $('td').css('background-image', ) ? win++ : win;
   // })
-  console.log('this function is called');
-  if(
-    $('#1').css('background-image') === "url(http://localhost:3000/assets/3/1.jpg)" &&
-    $('#2').css('background-image') === "url(http://localhost:3000/assets/3/2.jpg)" &&
-    $('#3').css('background-image') === "url(http://localhost:3000/assets/3/3.jpg)" &&
-    $('#4').css('background-image') === "url(http://localhost:3000/assets/3/4.jpg)" &&
-    $('#5').css('background-image') === "url(http://localhost:3000/assets/3/5.jpg)" &&
-    $('#6').css('background-image') === "url(http://localhost:3000/assets/3/6.jpg)" &&
-    $('#7').css('background-image') === "url(http://localhost:3000/assets/3/7.jpg)" &&
-    $('#8').css('background-image') === "url(http://localhost:3000/assets/3/8.jpg)" &&
-    $('#9').css('background-image') === "url(http://localhost:3000/assets/3/9.jpg)"
-  ) {
-    alert('you won!');
-    }
- }
+ //  console.log('this function is called');
+ //  if(
+ //    $('#1').css('background-image') === "url(http://localhost:3000/assets/3/1.jpg)" &&
+ //    $('#2').css('background-image') === "url(http://localhost:3000/assets/3/2.jpg)" &&
+ //    $('#3').css('background-image') === "url(http://localhost:3000/assets/3/3.jpg)" &&
+ //    $('#4').css('background-image') === "url(http://localhost:3000/assets/3/4.jpg)" &&
+ //    $('#5').css('background-image') === "url(http://localhost:3000/assets/3/5.jpg)" &&
+ //    $('#6').css('background-image') === "url(http://localhost:3000/assets/3/6.jpg)" &&
+ //    $('#7').css('background-image') === "url(http://localhost:3000/assets/3/7.jpg)" &&
+ //    $('#8').css('background-image') === "url(http://localhost:3000/assets/3/8.jpg)" &&
+ //    $('#9').css('background-image') === "url(http://localhost:3000/assets/3/9.jpg)"
+ //  ) {
+ //    alert('you won!');
+ //    }
+ // }
